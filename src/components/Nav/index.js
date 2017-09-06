8e3c3120a1361c5b4acf6d1ef4be9ed310b5f967
jesperengstrom <jengstro@gmail.com>
2017-09-07 01:15:28 +0200

    scrollableAnchor smooth scroll

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -6,15 +6,10 @@ import './styles.css';
 
 function Nav({ next, last }){
 
-    function smoothScroll(e){
-        e.preventDefault();
-        document.querySelector('#' + next).scrollIntoView({ behavior: 'smooth' });
-    }
-
     return (
         <div className="nav flex justify-center">
             <div className="fixed flex align-end p-1" style={{height:'33%'}}>
-                <a onClick={smoothScroll} href={'#' + next} className={`transition-flip ${last ? 'flip' : ''}`}>
+                <a href={'#' + next} className={`transition-flip ${last ? 'flip' : ''}`}>
                     <Icon name='arrow down'link size='huge' />
                 </a>
             </div>
--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,23 +1,26 @@
 import React from 'react';
-import { Grid } from 'semantic-ui-react';
+
+//libraries
+import ScrollableAnchor from 'react-scrollable-anchor';
 import VisibilitySensor from 'react-visibility-sensor';
 
 //CSS
 import './styles.css';
 
-export function Section({ section, setSection, children }) {
+function Section({ section, setSection, children}){
 
     function handleChange(status){
         if (status) {
             setSection(section);
         }
     }
-    return (
-        <Grid.Column id={section.id} className="border section">
-            {children}
-            <VisibilitySensor partialVisibility={true} minTopValue={250} onChange={handleChange}/>
-        </Grid.Column>
-    );
-}
+        return (
+        <ScrollableAnchor id={section.id}>
+            <div className="section border">
+                {children}
+                <VisibilitySensor partialVisibility={true} minTopValue={250} onChange={handleChange}/>
+            </div>
+        </ScrollableAnchor>
+    );}
 
 export default Section;
