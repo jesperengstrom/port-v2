6617ce7063aa02504732527f6a42b3a476c70687
jesperengstrom <jengstro@gmail.com>
2017-10-31 13:33:09 +0100

    mobile fixes

--- src/components/Section/About/About.js
+++ src/components/Section/About/About.js
@@ -8,8 +8,8 @@ const jesper = require('../../../img/j.jpg');
 
 function About(props) {
     return(
-        <article className="about-wrapper flex">
-            <div className="about-text-container item-text bold flex flex-column justify-center">
+        <article className="about-wrapper flex justify-center">
+            <div className="about-text-container item-text bold flex flex-column">
                 <p>Jag är en 35-årig Stockholmare som läser sista året på YH-utbildningen Front end-utvecklare på Nackademin.</p>
                 <p>Från början är jag journalist med många års erfarenhet som <a className="about-link" href='http://sverigesradio.se/sida/gruppsida.aspx?programid=4832&grupp=22908'>producent</a> och <a className="about-link" href='http://sverigesradio.se/sida/avsnitt/648150?programid=2519'>reporter</a> på Sveriges Radio.</p>
                 <p>Till våren 2018 söker jag en praktikplats som utvecklare. Om du tror att jag kan passa hos er, <a className="about-link" href='mailto:jengstro@gmail.com'>säg hej!</a></p>       
